088e3f77ade2de4541d15a57ed5e71bd6dafddff
lhori <nekoyukkuri@gmail.com>
2023-10-10 05:29:23 -0400

    inital commit

---
+++ tracks/api.py
@@ -0,0 +1,51 @@
+from typing import List, Optional
+from ninja import NinjaAPI
+from tracks.models import Track
+from tracks.schema import TrackSchema, NotFoundSchema
+
+# http://localhost:800/api/tracks
+
+api = NinjaAPI()
+
+@api.get("/tracks", response=List[TrackSchema])
+def tracks(request, title: Optional[str] = None):
+    if title:
+        return Track.objects.filter(title__icontains=title)
+    return Track.objects.all()
+
+
+@api.get("/tracks/{track_id}", response={200: TrackSchema, 404: NotFoundSchema})
+def track(request, track_id: int):
+    try:
+        track = Track.objects.get(pk=track_id)
+        return 200, track
+    except Track.DoesNotExist as e:
+        return 404, {"message": "Track does not exist"}
+    
+
+@api.post("/tracks", response={201: TrackSchema})
+def create_track(request, track: TrackSchema):
+    Track.objects.create(**track.dict())
+    return track
+
+
+@api.put("/tracks/{track_id}", response={200: TrackSchema, 404: NotFoundSchema})
+def change_track(request, track_id: int, data: TrackSchema):
+    try:
+        track = Track.objects.get(pk=track_id)
+        for attribute, value in data.dict().items():
+            setattr(track, attribute, value)
+        track.save()
+        return 200, track
+    except Track.DoesNotExist as e:
+        return 404, {"message": "Could not find track"}
+    
+    
+@api.delete("/tracks/{track_id}", response={200: None, 404: NotFoundSchema})
+def delete_track(request, track_id: int):
+    try:
+        track = Track.objects.get(pk=track_id)
+        track.delete()
+        return 200
+    except Track.DoesNotExist as e:
+        return 404, {"message": "Could not find track"}
